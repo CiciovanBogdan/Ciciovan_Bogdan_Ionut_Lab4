854a64b000c3d6ebb86bced3457e206180b2a5d7
BOGDAN-IONUȚ CICIOVAN <bogdan.ciciovan@stud.ubbcluj.ro>
2025-12-18 19:24:31 +0200

    sarcina laborator

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -1,25 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ciciovan_Bogdan_Ionut_Lab4;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML;
 using static Ciciovan_Bogdan_Ionut_Lab4.PricePredictionModel;
-//using static PricePrediction.MLModel;
+using static Ciciovan_Bogdan_Ionut_Lab4.TimePredictionModel;
 
 namespace PredictionController.Controllers
 {
     public class PredictionController : Controller
     {
-        public IActionResult Price(ModelInput input)
-        { 
+        public IActionResult Price(PricePredictionModel.ModelInput input)
+        {
             // Load the model
             MLContext mlContext = new MLContext();
-            
-            // Create predection engine related to the loaded train model
+
+            // Create prediction engine related to the loaded train model
             ITransformer mlModel = mlContext.Model.Load(@"PricePredictionModel.mlnet", out var modelInputSchema);
-            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
-            
+            var predEngine = mlContext.Model.CreatePredictionEngine<PricePredictionModel.ModelInput, PricePredictionModel.ModelOutput>(mlModel);
+
             // Try model on sample data to predict fair price
-            ModelOutput result = predEngine.Predict(input);
-            
+            PricePredictionModel.ModelOutput result = predEngine.Predict(input);
+
             ViewBag.Price = result.Score;
+            return View(input);
+        }
+
+        public IActionResult Time(TimePredictionModel.ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            // Create prediction engine related to the loaded train model
+            ITransformer mlModel = mlContext.Model.Load(@"TimePredictionModel.mlnet", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<TimePredictionModel.ModelInput, TimePredictionModel.ModelOutput>(mlModel);
+
+            // Try model on sample data to predict trip time
+            TimePredictionModel.ModelOutput result = predEngine.Predict(input);
+
+            ViewBag.Time = result.Score;
             return View(input);
         }
     }
