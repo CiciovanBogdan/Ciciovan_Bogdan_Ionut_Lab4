56e1d54f5b551f9df1c57ecc71e48c83dd053d93
BOGDAN-IONUȚ CICIOVAN <bogdan.ciciovan@stud.ubbcluj.ro>
2026-01-14 17:43:51 +0200

    laborator 10 + sarcina laborator 10

---
+++ Models/BatchPredictionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ciciovan_Bogdan_Ionut_Lab4.Models
+{
+    public class BatchPredictionViewModel
+    {
+        public IFormFile? File { get; set; }
+        // Rezultatele (predicțiile) pentru fiecare rând din fișier
+        public List<PricePredictionModel.ModelOutput>? Predictions { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
